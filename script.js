dda7ade5de8e4fe01aa2cf93fef0a7231baa1a4d
osamaasaid <osamaasaid@gmail.com>
2016-04-11 20:31:40 -0700

    added features to input

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 console.log("list open");
 
-document.getElementById('addBtn').addEventListener('click', function(e){
+var todoFunction = document.getElementById('addBtn').addEventListener('click', function(e){
 var newItem = document.createElement('li');
 var task = document.getElementById("item").value;
+ if (task === '') {
+  alert("cannot execute");
+ }
+var box = document.createElement('input');
+box.type = "checkbox";
 newItem.innerHTML = task;
 document.getElementById('todo').appendChild(newItem);
+document.getElementById('todo').appendChild(box);
+document.getElementById('item').value = '';
+document.getElementById('item').focus();
+console.log('clicked');
 });
+
+
 
 
 
