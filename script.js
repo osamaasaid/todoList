2ee6fd7f23fde7b2329c23747c52b1c4026aa6f7
osamaasaid <osamaasaid@gmail.com>
2016-04-13 15:13:28 -0700

    generating ids for new list items+ch.boxes

--- script.js
+++ script.js
@@ -1,23 +1,32 @@
 console.log("list open");
 
 var todoFunction = document.getElementById('addBtn').addEventListener('click', function(e){
-var newItem = document.createElement('li');
-var task = document.getElementById("item").value;
- if (task === '') {
-  alert("No item to add");
-  return false;
- }
-var box = document.createElement('input');
-box.type = "checkbox";
-newItem.innerHTML = task;
+  var newItem = document.createElement('li');
+     newItem.id= "newList";
+  var task = document.getElementById("item").value;
+      if (task === '') {
+      alert("No item to add");
+      return false;
+    }
+  newItem.innerHTML = task;
+  var box = document.createElement('input');
+  box.type = "checkbox";
+  box.id= "check";
 document.getElementById('todo').appendChild(newItem);
 document.getElementById('todo').appendChild(box);
+  var del = document.createElement("input");
+    del.type = "button";
+    del.id="remove";
+    document.getElementById('todo').appendChild(del);
 document.getElementById('item').value = '';
 document.getElementById('item').focus();
 console.log('clicked');
 });
 
-
+// var move = document.getElementById('check').addEventListener("click", function (event){
+//   var toMove = document.getElementById('newList');
+//   toMove.appendChild("done");
+// });
 
 
 
